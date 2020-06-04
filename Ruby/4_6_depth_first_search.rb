c6753e97d24205fbfd5772f3177efe4a56e27978
Murilo Roque <muriloengqui@gmail.com>
2020-06-04 11:52:42 -0300

    add 4_6 challenge

---
+++ Ruby/4_6_depth_first_search.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+def depth_first_search(graph)
+  discovered = Array.new(graph.keys.length, false)
+  path = []
+  stack = []
+  stack.push(0)
+  discovered[0] = true
+  stacking(stack, path, graph, discovered)
+end
+
+def stacking(stack, path, graph, discovered)
+  until stack.empty?
+    current = stack.pop
+    path.push(current)
+    graph[current].reverse_each do |adjacent|
+      stack.push(adjacent) unless discovered[adjacent]
+      discovered[adjacent] = true
+    end
+  end
+  path
+end
