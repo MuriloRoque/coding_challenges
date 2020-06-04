6b9afcaada414f27d1ba9fad753ed3091a0de16e
Murilo Roque <muriloengqui@gmail.com>
2020-06-04 12:11:19 -0300

    add 5_5 challenge

---
+++ Ruby/5_5_connected_components.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+def connected_graph?(graph)
+  discovered = Array.new(graph.keys.length, false)
+  stack = []
+  stack.push(0)
+  discovered[0] = true
+  discovered = stacking(stack, graph, discovered)
+  discovered.none?(false)
+end
+
+def stacking(stack, graph, discovered)
+  until stack.empty?
+    graph[stack.pop].reverse_each do |adjacent|
+      stack.push(adjacent) unless discovered[adjacent]
+      discovered[adjacent] = true
+    end
+  end
+  discovered
+end
