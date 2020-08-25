863e7c64896f114b70b9607bf90ff5b50fc11147
Murilo Roque <muriloengqui@gmail.com>
2020-08-25 04:38:40 -0300

    add merge K sorted lists

--- Python/MergeKSortedLists.py
+++ Python/MergeKSortedLists.py
@@ -1,10 +1,16 @@
+import heapq
+
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
 class Solution:
-    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+    def mergeKLists(self, lists: ListNode) -> ListNode:
         def vals(node):
             while node:
                 yield node.val
